36b72016cd4d4720524c98ed8003ecfe58f49ad4
GitHub Action <action@github.com>
2023-04-24 07:19:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/olegvinted/packwerkintellij/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/com/github/olegvinted/packwerkintellij/listeners/MyFrameStateListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.olegvinted.packwerkintellij.listeners
 
 import com.intellij.ide.FrameStateListener
 import com.intellij.openapi.diagnostic.thisLogger
