e9fe4410db80c44e04b8361495fe04c8cbd03975
Oleg Smelov <79197369+oleg-vinted@users.noreply.github.com>
2023-04-26 21:00:48 +0200

    Improve path handling

--- src/main/kotlin/com/vinted/packwerkintellij/annotators/PackwerkAnnotator.kt
+++ src/main/kotlin/com/vinted/packwerkintellij/annotators/PackwerkAnnotator.kt
@@ -4,9 +4,12 @@ import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.ExternalAnnotator
 import com.intellij.lang.annotation.HighlightSeverity
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.fileEditor.FileDocumentManager
-import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.openapi.roots.ProjectFileIndex
+import com.intellij.openapi.vfs.VfsUtilCore
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiDocumentManager
 import com.intellij.psi.PsiFile
 import org.intellij.markdown.lexer.push
@@ -16,7 +19,8 @@ import java.nio.charset.Charset
 import java.util.*
 
 const val PACKWERK_BINSTUB_PATH = "bin/packwerk"
-private val VIOLATION_PATTERN = Regex("^[^:]+:([0-9]+):([0-9]+)$")
+
+private val violationPattern = Regex("^[^:]+:([0-9]+):([0-9]+)$")
 
 internal class PackwerkAnnotator : ExternalAnnotator<PackwerkAnnotator.State, PackwerkAnnotator.Results>() {
     internal class State(var file: PsiFile)
@@ -29,10 +33,14 @@ internal class PackwerkAnnotator : ExternalAnnotator<PackwerkAnnotator.State, Pa
             return null
         }
 
-        val binstubFile = LocalFileSystem.getInstance().findFileByPath(
-            file.project.basePath + "/" + PACKWERK_BINSTUB_PATH
-        )
-        if (binstubFile == null || !binstubFile.exists()) {
+        val root = getRootForFile(file)
+        if (root == null) {
+            thisLogger().debug("Not linting because project root could not be determined")
+            return null
+        }
+
+        val binstubFile = root.findFileByRelativePath(PACKWERK_BINSTUB_PATH)
+        if (binstubFile == null) {
             thisLogger().debug("Not linting because Packwerk binstub was not found")
             return null
         }
@@ -40,24 +48,21 @@ internal class PackwerkAnnotator : ExternalAnnotator<PackwerkAnnotator.State, Pa
         return State(file)
     }
 
-    override fun doAnnotate(collectedInfo: State): Results {
-        val relativePath = collectedInfo.file.virtualFile.path
-                .removePrefix(collectedInfo.file.project.basePath.toString())
-                .removePrefix("/")
+    override fun doAnnotate(collectedInfo: State): Results? {
+        val root: VirtualFile = getRootForFile(collectedInfo.file) ?: return null
+        val relativePath = VfsUtilCore.getRelativePath(collectedInfo.file.virtualFile, root) ?: return null
 
         val cmd = GeneralCommandLine(PACKWERK_BINSTUB_PATH)
-        cmd.withWorkDirectory(collectedInfo.file.project.basePath)
-
-        cmd.charset = Charset.forName("UTF-8")
-        cmd.addParameter("check")
-        cmd.addParameter(relativePath)
+            .withWorkDirectory(root.path)
+            .withCharset(Charset.forName("UTF-8"))
+            .withParameters("check", relativePath)
 
         val process = cmd.createProcess()
         val scanner = Scanner(process.inputStream)
         val problems = ArrayList<Problem>()
 
         while (scanner.hasNextLine()) {
-            val matches = VIOLATION_PATTERN.matchEntire(scanner.nextLine())
+            val matches = violationPattern.matchEntire(scanner.nextLine())
             if (matches != null) {
                 val lineNumber = Integer.valueOf(matches.groupValues[1]) - 1
                 val columnNumber = Integer.valueOf(matches.groupValues[2]) - 1
@@ -99,5 +104,18 @@ internal class PackwerkAnnotator : ExternalAnnotator<PackwerkAnnotator.State, Pa
                 .range(el.textRange)
                 .create()
         }
+    }
+
+    private fun getRootForFile(file: PsiFile) : VirtualFile? {
+        val application = ApplicationManager.getApplication();
+        var root: VirtualFile? = null
+
+        application.runReadAction {
+            root = ProjectFileIndex
+                .getInstance(file.project)
+                .getContentRootForFile(file.virtualFile)
+        }
+
+        return root
     }
 }
